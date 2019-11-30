9e4ad96afa2150860d4691a0bfbf14541cc78038
noname <rekhlov.lev@mail.ru>
2019-11-30 15:17:53 +0300

    Готово главное меню, работает кнопка выхода

--- main.py
+++ main.py
@@ -5,41 +5,95 @@ pygame.init()
 
 class Button:
     def __init__(self, x, y, width, height, txt, sc):
-        self.bg = pygame.transform.scale(pygame.image.load('sources/button.png').convert(), (width, height))
-        self.bg.set_colorkey(WHITE)
+        self.bg = pygame.transform.scale(BTN_IMGS[0].convert(), (width, height))
         self.pos = (x, y)
+        self.x = x
+        self.y = y
         self.width = width
         self.height = height
-        self.text = txt
-        sc.blit(self.bg, self.pos)
-        font = pygame.font.Font(None, 50)
-        text = font.render(self.text, 1, WHITE)
-        sc.blit(text, (
-            self.pos[0] + (self.width - text.get_width()) // 2, self.pos[1] + (self.height - text.get_height()) // 2))
+        self.sc = sc
+        self.title = txt
+        self.text = pygame.font.SysFont('comicsansms', 30, bold=True).render(txt, 1, WHITE)
+        self.update()
+
+    def change_state(self):
+        if self.is_focused(pygame.mouse.get_pos()):
+            self.bg = pygame.transform.scale(BTN_IMGS[1].convert(), (self.width, self.height))
+        else:
+            self.bg = pygame.transform.scale(BTN_IMGS[0].convert(), (self.width, self.height))
+        self.update()
+
+    def update(self):
+        self.sc.blit(self.bg, self.pos)
+        self.sc.blit(self.text, (
+            self.pos[0] + (self.width - self.text.get_width()) // 2,
+            self.pos[1] + (self.height - self.text.get_height()) // 2))
+
+    def is_focused(self, pos):
+        x, y = pos
+        if self.x <= x <= self.x + self.width and self.y <= y <= self.y + self.height:
+            return True
+        return False
 
 
+WHITE = (255, 255, 255)
+BTN_IMGS = [pygame.image.load('sources/btn.png'), pygame.image.load('sources/btn_act.png')]
+BACKGROUND = pygame.image.load('sources/background.png')
+for btn in BTN_IMGS:
+    btn.set_colorkey(WHITE)
 WIN_WIDTH = 1280
 WIN_HEIGHT = 720
 FPS = 60
-WHITE = (255, 255, 255)
+MENU_SCREEN = 'menu_sc'
 
-clock = pygame.time.Clock()
 screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption('Simple Dungeon')
 pygame.display.set_icon(pygame.image.load('sources/icon.bmp').convert_alpha())
-main_screen = pygame.Surface((WIN_WIDTH, WIN_HEIGHT))
-# main_screen.blit(pygame.image.load('').convert(), (0, 0))
-button = Button(100, 50, 200, 100, 'test text', main_screen)
+clock = pygame.time.Clock()
 
-screen.blit(main_screen, (0, 0))
+now_screen = MENU_SCREEN
+menu_screen = pygame.Surface((WIN_WIDTH, WIN_HEIGHT))
+menu_screen.blit(BACKGROUND.convert(), (0, 0))
+title_size = 120
+title_text = pygame.font.SysFont('comicsansms', title_size, bold=True).render('Simple Dungeon', 1, WHITE)
+menu_screen.blit(title_text, ((WIN_WIDTH - title_text.get_width()) // 2, 5))
+
+SCREENS = {MENU_SCREEN: menu_screen}
+BUTTONS = [Button(WIN_WIDTH // 2 - 150, 250, 300, 100, 'НАЧАТЬ ИГРУ', menu_screen),
+           Button(WIN_WIDTH // 2 - 150, 400, 300, 100, 'О НАС', menu_screen),
+           Button(WIN_WIDTH // 2 - 150, 550, 300, 100, 'ВЫЙТИ', menu_screen)]
 
 running = True
+flag = True
 
 while running:
+    menu_screen.blit(BACKGROUND.convert(), (0, 0))
+    if now_screen == MENU_SCREEN:
+        if flag and title_size < 140:
+            title_size += 0.5
+        else:
+            flag = False
+        if not flag and title_size > 120:
+            title_size -= 0.5
+        else:
+            flag = True
+        title_text = pygame.font.SysFont('comicsansms', int(title_size), bold=True).render('Simple Dungeon', 1, WHITE)
+        menu_screen.blit(title_text, ((WIN_WIDTH - title_text.get_width()) // 2, 10))
+
+        for btn in BUTTONS:
+            btn.change_state()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        if now_screen == MENU_SCREEN:
+            for btn in BUTTONS:
+                if event.type == pygame.MOUSEBUTTONUP and event.button == 1 and btn.is_focused(event.pos):
+                    if btn.title == 'ВЫЙТИ':
+                        running = False
+
+    screen.blit(SCREENS[now_screen], (0, 0))
     pygame.display.flip()
     clock.tick(FPS)
 pygame.quit()
