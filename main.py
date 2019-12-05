29c1b6293dd0b0566da469fcf3837a1dca9d6dba
noname <rekhlov.lev@mail.ru>
2019-12-05 17:28:27 +0300

    ВСЁ РАБОТАЕТ, меняю код по спрайт никита гей

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
+import os
 import pygame
 
 pygame.init()
 
 
+def load_image(name, colorkey=None):
+    fullname = os.path.join('sources', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error:
+        print('Изображение', name, 'отсутствует.')
+        exit(0)
+    if colorkey:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image = image.convert()
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
 class Button:
     def __init__(self, x, y, width, height, txt, sc):
-        self.bg = pygame.transform.scale(BTN_IMGS[0].convert(), (width, height))
+        self.bg = pygame.transform.scale(BTN_IMGS[0], (width, height))
         self.pos = (x, y)
         self.x = x
         self.y = y
@@ -18,9 +36,9 @@ class Button:
 
     def change_state(self):
         if self.is_focused(pygame.mouse.get_pos()):
-            self.bg = pygame.transform.scale(BTN_IMGS[1].convert(), (self.width, self.height))
+            self.bg = pygame.transform.scale(BTN_IMGS[1], (self.width, self.height))
         else:
-            self.bg = pygame.transform.scale(BTN_IMGS[0].convert(), (self.width, self.height))
+            self.bg = pygame.transform.scale(BTN_IMGS[0], (self.width, self.height))
         self.update()
 
     def update(self):
@@ -38,10 +56,6 @@ class Button:
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-BTN_IMGS = [pygame.image.load('sources/btn.png'), pygame.image.load('sources/btn_act.png')]
-BACKGROUND = pygame.image.load('sources/background.png')
-for btn in BTN_IMGS:
-    btn.set_colorkey(WHITE)
 WIN_WIDTH = 1280
 WIN_HEIGHT = 720
 FPS = 60
@@ -53,14 +67,17 @@ pygame.display.set_caption('Simple Dungeon')
 pygame.display.set_icon(pygame.image.load('sources/icon.bmp').convert_alpha())
 clock = pygame.time.Clock()
 
+BTN_IMGS = [load_image('btn.png', WHITE), load_image('btn_act.png', WHITE)]
+BACKGROUND = load_image('background.png')
+
 now_screen = MENU_SCREEN
 
 about_screen = pygame.Surface((WIN_WIDTH, WIN_HEIGHT))
-about_screen.blit(BACKGROUND.convert(), (0, 0))
+about_screen.blit(BACKGROUND, (0, 0))
 titre_y = WIN_HEIGHT
 
 menu_screen = pygame.Surface((WIN_WIDTH, WIN_HEIGHT))
-menu_screen.blit(BACKGROUND.convert(), (0, 0))
+menu_screen.blit(BACKGROUND, (0, 0))
 title_size = 120
 title_text = pygame.font.SysFont('comicsansms', title_size, bold=True).render('Simple Dungeon', 1, WHITE)
 menu_screen.blit(title_text, ((WIN_WIDTH - title_text.get_width()) // 2, 5))
@@ -75,7 +92,7 @@ flag = True
 
 while running:
     if now_screen == MENU_SCREEN:
-        menu_screen.blit(BACKGROUND.convert(), (0, 0))
+        menu_screen.blit(BACKGROUND, (0, 0))
         if flag and title_size < 140:
             title_size += 0.5
         else:
@@ -90,7 +107,7 @@ while running:
         for btn in BUTTONS:
             btn.change_state()
     elif now_screen == ABOUT_SCREEN:
-        about_screen.blit(BACKGROUND.convert(), (0, 0))
+        about_screen.blit(BACKGROUND, (0, 0))
         text_y = 0
         with open('sources/titres.txt', encoding='utf-8') as f:
             for s in f.readlines():
