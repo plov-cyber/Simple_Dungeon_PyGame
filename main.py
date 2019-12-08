85946eb028907408ee7c3b194903cf869dc51995
plov-cyber <rekhlov.lev@mail.ru>
2019-12-08 12:28:49 +0300

    Добавил комментарии во всём коде.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Импорт необходимых библиотек
 import os
 import pygame
 
@@ -5,6 +6,7 @@ pygame.init()
 
 
 def load_image(name, colorkey=None):
+    """Загружает картинку. Использует colorkey в качестве фонового цвета или делает прозрачный фон."""
     fullname = os.path.join('sources', name)
     try:
         image = pygame.image.load(fullname)
@@ -22,6 +24,8 @@ def load_image(name, colorkey=None):
 
 
 class Button(pygame.sprite.Sprite):
+    """Класс кнопки. Принимает значение ординаты y и текст самой кнопки."""
+
     def __init__(self, group, y, txt):
         super().__init__(group)
         self.image = BTN_IMGS[0]
@@ -31,6 +35,7 @@ class Button(pygame.sprite.Sprite):
         self.text = pygame.font.SysFont('comicsansms', 40, bold=True).render(txt, 1, pygame.Color('white'))
 
     def update(self):
+        """Меняет фоновое изображение кнопки при наведении на неё."""
         if self.rect.collidepoint(pygame.mouse.get_pos()):
             self.image = BTN_IMGS[1]
         else:
@@ -39,6 +44,7 @@ class Button(pygame.sprite.Sprite):
                                      self.rect.y + (self.rect.height - self.text.get_height()) // 2))
 
 
+# Константы.
 WIN_WIDTH = 1280
 WIN_HEIGHT = 720
 FPS = 60
@@ -46,18 +52,22 @@ MENU_SCREEN = 'menu_screen'
 ABOUT_SCREEN = 'about_screen'
 now_screen = MENU_SCREEN
 
+# Инициализация экрана, установка оглавление и иконки приложения.
 screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption('Simple Dungeon')
 pygame.display.set_icon(pygame.image.load('sources/icon.bmp').convert_alpha())
 clock = pygame.time.Clock()
 
+# Загрузка изображений.
 BTN_IMGS = [load_image('btn.png', pygame.Color('white')), load_image('btn_act.png', pygame.Color('white'))]
 BACKGROUND = load_image('background.png')
 
+# Экран с титрами(О НАС).
 about_screen = pygame.Surface((WIN_WIDTH, WIN_HEIGHT))
 about_screen.blit(BACKGROUND, (0, 0))
 titre_y = WIN_HEIGHT
 
+# Главное меню игры.
 menu_screen = pygame.Surface((WIN_WIDTH, WIN_HEIGHT))
 menu_screen.blit(BACKGROUND, (0, 0))
 title_size = 120
@@ -66,15 +76,20 @@ title_text = pygame.font.SysFont('comicsansms', title_size, bold=True).render('S
 menu_screen.blit(title_text, ((WIN_WIDTH - title_text.get_width()) // 2, 5))
 
 SCREENS = {MENU_SCREEN: menu_screen, ABOUT_SCREEN: about_screen}
+
+# Инициализация кнопок и объединение их в группу.
 BTN_TITLES = ['НАЧАТЬ ИГРУ', 'О НАС', 'ВЫЙТИ']
 buttons = pygame.sprite.Group()
 for i in range(3):
     button = Button(buttons, i * 150 + 250, BTN_TITLES[i])
 
+# Флаги.
 running = True
 flag = True
 
+# Главный цикл игры.
 while running:
+    # Код для главного меню игры.
     if now_screen == MENU_SCREEN:
 
         for event in pygame.event.get():
@@ -102,6 +117,7 @@ while running:
         buttons.draw(menu_screen)
         buttons.update()
 
+    # Код для экрана с титрами.
     elif now_screen == ABOUT_SCREEN:
 
         for event in pygame.event.get():
@@ -119,6 +135,7 @@ while running:
                 text_y += text.get_height()
         titre_y -= 1
 
+    # Обновление экрана
     screen.blit(SCREENS[now_screen], (0, 0))
     pygame.display.flip()
     clock.tick(FPS)
