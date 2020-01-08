7abbd329c796cac6f0f8898fe47ae615e7fd9ce9
noname <rekhlov.lev@mail.ru>
2020-01-08 13:27:44 +0300

    Добавил GAME OVER. Добавил бег игрока на LShift. Написал несколько вспомогательных функций. Добавил персонажу аттаку.

--- main.py
+++ main.py
@@ -6,13 +6,29 @@ import random
 pygame.init()
 
 
-def escape():
+def pause():
+    """Функция для паузы игры."""
+
+    global now_screen, pause_bg
+    now_screen = PAUSE_SCREEN
+    pause_bg = lvl_sc.copy()
+
+
+def game_over():
+    """Функция для выхода в главное меню после проигрыша."""
+
+    global now_screen, game_over_bg
+    now_screen = GAME_OVER
+    game_over_bg = lvl_sc.copy()
+    reset_level()
+
+
+def reset_level():
     """Функция выхода с уровня."""
-    global now_screen
+
     pygame.mouse.set_visible(True)
-    now_screen = LVL_CH_SCREEN
-    now_level.kill()
-    hero.kill()
+    now_level.reset()
+    hero.reset()
 
 
 def terminate():
@@ -41,6 +57,7 @@ def load_sound(type, name):
 
 def load_image(name, colorkey=None):
     """Загружает картинку. Использует colorkey в качестве фонового цвета или делает прозрачный фон."""
+
     fullname = os.path.join('sources', name)
     try:
         image = pygame.image.load(fullname)
@@ -58,34 +75,53 @@ def load_image(name, colorkey=None):
 
 
 class Monster(pygame.sprite.Sprite):
+    """Класс монстра. Принимает значения координаты, атаки, здоровья."""
+
     def __init__(self, x, dmg, health, d):
-        super().__init__(enemies, all_sprites)
+        """Инициализация монстра."""
+
+        super().__init__(monsters, all_sprites)
         self.image = random.choice(MNSTR_IMGS)
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = x, Y0 + hero.rect.height - self.rect.height
         self.x0 = x
         self.vx = 1
+        self.death_vy = 2
         self.dmg = dmg
         self.health = health
+        self.full_health = health
         self.range = d
         self.right = True
         self.ticks = 0
 
     def update(self):
-        collider = pygame.sprite.spritecollideany(self, hero_group)
-        if not collider:
-            if self.right and self.x0 + self.range > self.rect.x:
-                self.rect.x += self.vx
-            else:
-                self.right = False
+        """Двигает мостра в пределах указанных координат. Наносит урон игроку."""
 
-            if not self.right and self.rect.x > self.x0:
-                self.rect.x -= self.vx
-            else:
-                self.right = True
-        elif self.ticks % 30 == 0:
-            collider.health -= self.dmg
-        self.ticks = (self.ticks + 1) % 120
+        if self.health <= 0:
+            self.image.set_alpha(250)
+            self.rect.y += self.death_vy
+        else:
+
+            collider = pygame.sprite.spritecollideany(self, hero_group)
+            if not collider:
+                if self.right and self.x0 + self.range > self.rect.x:
+                    self.rect.x += self.vx
+                else:
+                    self.right = False
+
+                if not self.right and self.rect.x > self.x0:
+                    self.rect.x -= self.vx
+                else:
+                    self.right = True
+            elif self.ticks % 30 == 0:
+                collider.health -= self.dmg
+            self.ticks = (self.ticks + 1) % 120
+
+    def reset(self):
+        self.health = self.full_health
+        self.rect.x = self.x0
+        self.right = True
+        self.ticks = 0
 
 
 class Platform(pygame.sprite.Sprite):
@@ -93,6 +129,7 @@ class Platform(pygame.sprite.Sprite):
 
     def __init__(self, x, y):
         """Инициализация платформы."""
+
         super().__init__(platforms, all_sprites)
         self.image = PLATF_IMG
         self.rect = self.image.get_rect()
@@ -105,6 +142,7 @@ class Cloud(pygame.sprite.Sprite):
 
     def __init__(self):
         """Инициализация облака."""
+
         super().__init__(clouds, all_sprites)
         self.image = random.choice(CLOUD_IMGS)
         self.rect = self.image.get_rect()
@@ -114,6 +152,7 @@ class Cloud(pygame.sprite.Sprite):
 
     def update(self):
         """Обновление координат облака."""
+
         if self.rect.x < now_level.rect.x - self.rect.width or self.rect.x > 6144:
             self.kill()
         else:
@@ -125,16 +164,18 @@ class Level(pygame.sprite.Sprite):
 
     def __init__(self, image, enemies, platforms):
         """Инициализация уровня."""
+
         super().__init__(level, all_sprites)
         self.image = image
         self.diff = 0
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = 0, -500
-        self.enemies = enemies
-        self.platforms = platforms
+        self.enemies = pygame.sprite.Group(enemies)
+        self.platforms = pygame.sprite.Group(platforms)
 
     def update(self):
         """Обновление всех спрайтов уровня."""
+
         if 625 <= hero.x <= 5489:
             self.diff = hero.x - hero.rect.x + self.rect.x
             self.rect.x = -(hero.x - hero.rect.x)
@@ -148,18 +189,28 @@ class Level(pygame.sprite.Sprite):
             en.rect.x -= self.diff
             en.x0 -= self.diff
 
+    def reset(self):
+        """Сброс уровня."""
+
+        self.diff = 0
+        self.rect.x = 0
+        for enemy in self.enemies:
+            enemy.reset()
+
 
 class Hero(pygame.sprite.Sprite):
     """Класс главного героя. Принимает координаты."""
 
     def __init__(self, group, x, y, dmg, health):
         """Инициализация персонажа"""
+
         super().__init__(group)
         self.image = PLAYER_IMGS[2]
         self.img_count = 0
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = x, y
         self.x = x
+        self.pos0 = (x, y)
         self.safe_x = (self.x, self.rect.x)
         self.y0 = Y0
         self.left = False
@@ -169,12 +220,15 @@ class Hero(pygame.sprite.Sprite):
         self.jump_height = 50
         self.jump_count = 0
         self.vx = 4
+        self.full_health = health
         self.health = health
+        self.freq = 15
         self.ticks = 0
         self.dmg = dmg
 
     def move(self, x, y):
         """Движение персонажа."""
+
         self.moving = True
         if x < self.x and not self.left:
             self.left = True
@@ -195,8 +249,9 @@ class Hero(pygame.sprite.Sprite):
 
     def update(self):
         """Обновление координат персонажа при взаимодействии с ним."""
-        if self.health <= 0:
-            escape()
+
+        if self.health <= 0 or self.rect.y == 530:
+            game_over()
 
         self.platform = False
         for platform in now_level.platforms:
@@ -222,7 +277,7 @@ class Hero(pygame.sprite.Sprite):
             self.jump = False
             self.jump_count = 0
 
-        if self.moving and self.ticks % 10 == 0:
+        if self.moving and self.ticks % self.freq == 0:
             self.image = pygame.transform.flip(PLAYER_IMGS[self.img_count], self.left, 0)
             self.img_count = (self.img_count + 1) % 2
             self.moving = False
@@ -230,13 +285,40 @@ class Hero(pygame.sprite.Sprite):
             self.image = pygame.transform.flip(PLAYER_IMGS[2], self.left, 0)
         self.ticks = (self.ticks + 1) % 120
 
+    def attack(self):
+        """Функция атаки персонажа."""
+
+        enemy = pygame.sprite.spritecollideany(self, now_level.enemies)
+        if enemy and self.ticks % self.freq == 0:
+            enemy.health -= self.dmg
+        if self.ticks % (self.freq // 2) == 0:
+            self.image = pygame.transform.flip(PLAYER_IMGS[self.img_count + 2], self.left, 0)
+            self.img_count = (self.img_count + 1) % 2
+        self.ticks = (self.ticks + 1) % 120
+
+    def reset(self):
+        """Сброс персонажа."""
+
+        self.ticks = 0
+        self.jump_count = 0
+        self.y0 = Y0
+        self.rect.x, self.rect.y = self.pos0
+        self.x = self.pos0[0]
+        self.safe_x = (self.x, self.rect.x)
+        self.left = False
+        self.jump = False
+        self.platform = False
+        self.moving = False
+        self.health = self.full_health
+
 
 class Button(pygame.sprite.Sprite):
     """Класс кнопки. Принимает значение ординаты y, текст и цвет текста самой кнопки."""
 
     def __init__(self, screen, group, y, txt, txt_color):
-        """Инициализация экземпляра."""
-        super().__init__(group)
+        """Инициализация экземпляра кнопки."""
+
+        super().__init__(group, all_sprites)
         self.color = txt_color
         self.screen = screen
         self.image = BTN_IMGS[0]
@@ -247,6 +329,7 @@ class Button(pygame.sprite.Sprite):
 
     def update(self):
         """Меняет фоновое изображение кнопки при наведении на неё."""
+
         if self.rect.collidepoint(pygame.mouse.get_pos()):
             self.image = BTN_IMGS[1]
         else:
@@ -265,6 +348,7 @@ ABOUT_SCREEN = 'about_screen'
 LVL_CH_SCREEN = 'level_choice_screen'
 LVL = 'level_screen'
 GAME_OVER = 'game_over'
+PAUSE_SCREEN = 'pause_screen'
 now_screen = MENU_SCREEN
 with open('sources/titres.txt', encoding='utf-8') as f:
     TITRES_TEXT = [i.strip() for i in f.readlines()]
@@ -272,16 +356,18 @@ BUTTON_FONT = pygame.font.SysFont('comicsansms', 40, bold=True)
 TITRES_FONT = pygame.font.SysFont('comicsansms', 50)
 MENU_BTN_TITLES = ['НАЧАТЬ ИГРУ', 'О НАС', 'ВЫЙТИ']
 LEVEL_BTN_TITLES = ['Уровень 1', 'Уровень 2', 'Уровень 3']
+PAUSE_BTN_TITLES = ['ПРОДОЛЖИТЬ', 'В ГЛАВНОЕ МЕНЮ']
 
 # Инициализация групп спрайтов.
 all_sprites = pygame.sprite.Group()
+pause_buttons = pygame.sprite.Group()
 menu_buttons = pygame.sprite.Group()
 level_buttons = pygame.sprite.Group()
 hero_group = pygame.sprite.Group()
 level = pygame.sprite.Group()
 clouds = pygame.sprite.Group()
 platforms = pygame.sprite.Group()
-enemies = pygame.sprite.Group()
+monsters = pygame.sprite.Group()
 
 # Инициализация экрана, установка оглавления и иконки приложения.
 screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
@@ -298,13 +384,12 @@ CLOUD_IMGS = [load_image('cloud1.png', (95, 205, 228)), load_image('cloud2.png',
 PLATF_IMG = pygame.transform.scale(load_image('platform.png', (95, 205, 228)), (54, 16))
 PLAYER_IMGS = [pygame.transform.scale(load_image('knight1.png'), (30, 80)),
                pygame.transform.scale(load_image('knight2.png'), (30, 80)),
-               pygame.transform.scale(load_image('knight.png'), (30, 80))]
+               pygame.transform.scale(load_image('knight.png'), (30, 80)),
+               pygame.transform.scale(load_image('slime.png'), (30, 80))]
 MNSTR_IMGS = [load_image('slime.png')]
+LVL_IMGS = [load_image('level_1.png')]
 
 # Загрузка звуков и музыки.
-load_sound(0, 'title_menu_music.mp3')
-pygame.mixer_music.set_volume(0.3)
-pygame.mixer_music.play(-1)
 btn_sound = load_sound(1, 'btn_sound.wav')
 
 # Главный герой.
@@ -312,44 +397,50 @@ hero = Hero(hero_group, 5, 445, 30, 100)
 
 # Экран с титрами(О НАС).
 about_screen = pygame.Surface((WIN_WIDTH, WIN_HEIGHT))
-about_screen.blit(BACKGROUND, (0, 0))
 titre_y = WIN_HEIGHT
 
 # Главное меню игры.
 menu_screen = pygame.Surface((WIN_WIDTH, WIN_HEIGHT))
-menu_screen.blit(BACKGROUND, (0, 0))
 title_size = 120
 title_text = pygame.font.SysFont('comicsansms', title_size, bold=True).render('Simple Dungeon', 1,
                                                                               pygame.Color('white'))
-menu_screen.blit(title_text, ((WIN_WIDTH - title_text.get_width()) // 2, 5))
 
 # Экран для выбора уровня.
 l_ch_screen = pygame.Surface((WIN_WIDTH, WIN_HEIGHT))
-l_ch_screen.blit(BACKGROUND, (0, 0))
 LVL_CH_TITLE = pygame.font.SysFont('comicsansms', 120, bold=True).render('Выберите уровень', 1,
                                                                          pygame.Color('black'))
-l_ch_screen.blit(LVL_CH_TITLE, ((WIN_WIDTH - LVL_CH_TITLE.get_width()) // 2, 5))
 
 # Уровень.
 lvl_sc = pygame.Surface((WIN_WIDTH, WIN_HEIGHT))
 level_x = 0
 now_level = None
-LEVELS = [Level(pygame.transform.scale(load_image('level_1.png'), (6144, 1440)), [Monster(600, 10, 50, 200)],
+LEVELS = [Level(pygame.transform.scale(LVL_IMGS[0], (6144, 1440)), [Monster(710, 10, 50, 200)],
                 [Platform(2306, 496), Platform(2405, 476), Platform(2497, 454)])]
 ticks = 0
-hero.kill()
 
 # Экран для game over'а.
 game_over_sc = pygame.Surface((WIN_WIDTH, WIN_HEIGHT))
+game_over_sc.set_alpha(150)
+game_over_bg = LVL_IMGS[0]
+fog = pygame.Surface((WIN_WIDTH, WIN_HEIGHT))
+fog.fill((0, 0, 0))
+fog.set_alpha(150)
+GAME_OVER_TITLE = pygame.font.SysFont('comicsansms', 180, bold=True).render('GAME OVER', 1, pygame.Color('white'))
+game_over_txt = pygame.font.SysFont('comicsansms', 50).render('Нажмите любую клавишу', 1, pygame.Color('white'))
+
+# Экран паузы.
+pause_sc = pygame.Surface((WIN_WIDTH, WIN_HEIGHT))
 
 # Экраны.
 SCREENS = {MENU_SCREEN: menu_screen, ABOUT_SCREEN: about_screen, LVL_CH_SCREEN: l_ch_screen, LVL: lvl_sc,
-           GAME_OVER: game_over_sc}
+           GAME_OVER: game_over_sc, PAUSE_SCREEN: pause_sc}
 
 # Инициализация кнопок и объединение их в группы.
 for i in range(3):
     Button(menu_screen, menu_buttons, i * 150 + 250, MENU_BTN_TITLES[i], pygame.Color('white'))
     Button(l_ch_screen, level_buttons, i * 150 + 250, LEVEL_BTN_TITLES[i], pygame.Color('black'))
+    if i < 2:
+        Button(pause_sc, pause_buttons, i * 150 + 300, PAUSE_BTN_TITLES[i], pygame.Color('grey'))
 
 # Флаги.
 running = True
@@ -426,11 +517,10 @@ while running:
             for btn in level_buttons:
                 if event.type == pygame.MOUSEBUTTONUP and event.button == 1 and btn.rect.collidepoint(event.pos):
                     btn_sound.play()
-                    pygame.mouse.set_visible(False)
-                    hero = Hero(hero_group, 5, 445, 30, 100)
-                    now_screen = LVL
                     if btn.title == 'Уровень 1':
                         now_level = LEVELS[0]
+                        pygame.mouse.set_visible(False)
+                        now_screen = LVL
                     elif btn.title == 'Уровень 2':
                         pass
                     elif btn.title == 'Уровень 3':
@@ -450,28 +540,52 @@ while running:
             if event.type == pygame.QUIT:
                 terminate()
             elif event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE:
-                escape()
+                pause()
 
         keys = pygame.key.get_pressed()
-        if (keys[pygame.K_SPACE] or keys[pygame.K_w]) and hero.rect.y == hero.y0:
+        if keys[pygame.K_SPACE] and hero.rect.y == hero.y0:
             hero.jump = True
         if keys[pygame.K_d]:
             hero.move(hero.x + hero.vx, hero.rect.y)
         if keys[pygame.K_a]:
             hero.move(hero.x - hero.vx, hero.rect.y)
+        if keys[pygame.K_TAB]:
+            hero.rect.y -= 100
+        if keys[pygame.K_LSHIFT]:
+            hero.vx = 5
+            hero.freq = 10
+        else:
+            hero.vx = 3
+            hero.freq = 15
+        if keys[pygame.K_w] and not(keys[pygame.K_d] or keys[pygame.K_a]):
+            hero.attack()
 
         level.draw(lvl_sc)
-        platforms.draw(lvl_sc)
-        enemies.draw(lvl_sc)
+        now_level.platforms.draw(lvl_sc)
+        now_level.enemies.draw(lvl_sc)
         hero_group.draw(lvl_sc)
         if ticks == random.randint(0, 75):
             Cloud()
         clouds.draw(lvl_sc)
         hero_group.update()
-        enemies.update()
+        now_level.enemies.update()
         level.update()
         clouds.update()
         ticks = (ticks + 1) % 75
+
+    # Код для GAME OVER.
+    elif now_screen == GAME_OVER:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type in [pygame.MOUSEBUTTONUP, pygame.KEYUP]:
+                now_screen = MENU_SCREEN
+
+        game_over_sc.blit(game_over_bg, (0, 0))
+        game_over_sc.blit(fog, (0, 0))
+        game_over_sc.blit(GAME_OVER_TITLE, ((WIN_WIDTH - GAME_OVER_TITLE.get_width()) // 2, 80))
+        game_over_sc.blit(game_over_txt, ((WIN_WIDTH - game_over_txt.get_width()) // 2, 300))
 
     # Обновление экрана
     screen.blit(SCREENS[now_screen], (0, 0))
